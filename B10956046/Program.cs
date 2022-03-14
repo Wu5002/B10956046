5313e1297e6c68736e374c5766d58b6aff27951f
user <user@DESKTOP-6JBEF5K>
2022-03-14 10:29:43 +0800

    1

---
+++ B10956046/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace B10956046
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("請輸入一個整數");
+            string user = Console.ReadLine();
+            int num = Convert.ToInt32(user);
+            if(num%2==1)
+            {
+                Console.WriteLine("Weird");
+            }else if(num%2 ==0)
+            {
+                if(num >=2 && num <=5 || num > 20)
+                {
+                    Console.WriteLine("Not Weird");
+                }
+                if(num >=6 && num > 20)
+                {
+                    Console.WriteLine("Weird");
+                }
+            }
+        }
+    }
+}
